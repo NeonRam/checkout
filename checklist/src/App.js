44def3a189deb397a0c0eca1cfe3f0ebb2240a9a
NeonRam <Ram1055@hotmail.com>
2022-07-26 14:12:47 +0000

    first commit

---
+++ checklist/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Checkout from './componenents/checkout';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout/>
+    </div>
+  );
+}
+
+export default App;
